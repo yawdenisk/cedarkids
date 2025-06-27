b4cdc134c23340ab72d96f56f348d2e3b5d16e75
yawdenisk <yawdenisk@gmail.com>
2025-06-27 18:50:54 +0200

    reformat all product details

--- frontend/woodlit/src/components/ProductDetails.js
+++ frontend/woodlit/src/components/ProductDetails.js
@@ -7,21 +7,22 @@ import warranty from '../images/warranty.webp';
 import cert from '../images/cert.webp';
 import ReviewForm from './ReviewForm';
 import userIcon from '../images/user.png'
+import config from '../config'
 
 export default function ProductDetails({cart, setCart}) {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
-    const [activeTab, setActiveTab] = useState('description');
     const [gallery, setGallery] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
     const [thumbsStartIndex, setThumbsStartIndex] = useState(0);
     const [installation, setInstallation] = useState(false);
     const [showReviewForm, setShowReviewForm] = useState(false);
     const [showImage, setShowImage] = useState(false);
-    const [currentImage, setCurrentImage] = useState();
+    const [currentImage
+        , setCurrentImage] = useState();
 
     useEffect(() => {
-        axios.get(`http://localhost:8081/api/product/get/${id}`)
+        axios.get(`${config.API_URL}/api/product/get/${id}`)
             .then(response => {
                 setProduct(response.data);
                 const images = [response.data.image, ...response.data.gallery.map(img => img.imageUrl)];
@@ -92,164 +93,166 @@ export default function ProductDetails({cart, setCart}) {
     };
 
     return (
-        <div className='container'>
-            <div className='productDetails'>
-                <div className='view'>
-                    <div className='img-area'>
-                        <button className="prev" onClick={prevSlide}>&#10094;</button>
-                    <img onClick={() => {
-                        setCurrentImage(gallery[currentSlide]);
-                        setShowImage(true)
-                    }} src={gallery[currentSlide]} alt="Product"/>
-                    <button className="next" onClick={nextSlide}>&#10095;</button>
-                    </div>
-                    <div className="thumbnail-gallery">
-                        <button
-                            className="thumb-prev"
-                            onClick={() => setThumbsStartIndex(Math.max(0, thumbsStartIndex - 1))}
-                            disabled={thumbsStartIndex === 0}
-                        >
-                            &#10094;
-                        </button>
-                        <div className="thumbs-container">
-                            <div className="thumbs-wrapper"
-                                 style={{transform: `translateX(-${thumbsStartIndex * 160}px)`}}>
-                                {gallery.map((img, index) => (
-                                    <img key={index}
-                                         src={img}
-                                         alt="Thumbnail"
-                                         className={currentSlide === index ? "active-thumbnail" : ""}
-                                         onClick={() => selectImage(index)}
-                                    />
-                                ))}
-                            </div>
+        <>             {showImage && (
+            <div className='imageContainer' onClick={() => setShowImage(false)}>
+                <button className='closeButton' onClick={() => setShowImage(false)}>✕</button>
+                <img src={currentImage} alt="Full View"/>
+            </div>
+        )}
+            <div className='container'>
+                <div className='productDetails'>
+                    <div className='view'>
+                        <div className='img-area'>
+                            <button className="prev" onClick={prevSlide}>&#10094;</button>
+                            <img onClick={() => {
+                                setCurrentImage(gallery[currentSlide]);
+                                setShowImage(true)
+                            }} src={gallery[currentSlide]} alt="Product"/>
+                            <button className="next" onClick={nextSlide}>&#10095;</button>
                         </div>
-                        <button
-                            className="thumb-next"
-                            onClick={() => setThumbsStartIndex(Math.min(gallery.length - maxVisibleThumbs, thumbsStartIndex + 1))}
-                            disabled={thumbsStartIndex >= gallery.length - maxVisibleThumbs}
-                        >
-                            &#10095;
-                        </button>
-                    </div>
-                </div>
-
-                <div className='information'>
-                    <h1>{product.name}</h1>
-                    <div className='price'>
-                        <p>€ {(product.price).toFixed(2)}</p>
-                        <s>€ {(product.lastPrice).toFixed(2)}</s>
-                    </div>
-                    <div className='installation'>
-                        <input type='checkbox' checked={installation} onChange={handleChangeCheckbox}></input>
-                        <p>installation + € {(product.installationPrice).toFixed(2)}</p>
-                    </div>
-                    <button onClick={() => addToCart(product.id, installation)}>ADD TO CART</button>
-                    <div className="description">
-                        <p>{product.description}</p>
-                    </div>
-                </div>
-
-            </div>
-            <p style={{textAlign: 'center', fontSize: '25px', marginTop: '50px'}}>WHY CEDARKIDS SWING SETS?</p>
-            <ul className='whyBlock'>
-                <li>
-                    <img src={quality}/>
-                    <h1>High Quality Design</h1>
-                    <p>We work hard to make sure your swing set features design elements as stylish as they are
-                        strong. From the materials to the colors to the overall aesthetic, these are swing sets
-                        you'll be proud to have in your backyard. </p>
-                </li>
-                <li>
-                    <img src={cert}/>
-                    <h1>Tested and Certified</h1>
-                    <p>All of our swing sets are certified to meet and exceed ASTM standards. We test our
-                        performance for kids up to 12-years-old, going above and beyond industry standards.</p>
-                </li>
-                <li>
-                    <img src={warranty}/>
-                    <h1>Warranty and Assembly</h1>
-                    <p>With a 5 Year Limited Warranty and 3D interactive assembly instructions with the BILT® app,
-                        you'll be supported from the very beginning for years and years of backyard fun.</p>
-                </li>
-            </ul>
-            <div className='demensions'>
-                <p>Assembled Dimensions</p>
-                <p>{formattedDemensions}</p>
-                <p>This product is intended for RESIDENTIAL USE ONLY. Any use of this product outside of a residential
-                    setting will make the product warranty null and void.</p>
-            </div>
-
-            <div className='composition'>
-                <img src={product.compositionImage} alt="Composition"/>
-                <ul>
-                    {product.composition && product.composition.split("\n").map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
-            </div>
-
-            <div className='reviewDetails'>
-                <p>CUSTOMER REVIEWS</p>
-
-                {[1, 2, 3, 4, 5].map((star) => (
-                    <span
-                        key={star}
-                        style={{
-                            color: averageRate >= star ? 'gold' : 'gray',
-                            fontSize: '24px',
-                        }}
-                    >
-                              ★
-                            </span>
-                ))}
-                <span> /</span>
-                <span style={{color: 'gold', fontSize: '24px'}}> ★★★★★</span>
-                <p>{averageRate.toFixed(2)} / 5.00</p>
-                <p>Based on {product.reviews.length} reviews</p>
-                <button onClick={() => setShowReviewForm(!showReviewForm)}>Write a review</button>
-            </div>
-            {showReviewForm &&
-                <ReviewForm setShowReviewForm={setShowReviewForm} product={product}/>}
-            <div className='reviews'>
-                {product.reviews.map(item => (
-                    <div className='comment'>
-                        <div className='userInfo'>
-                            <img src={userIcon}/>
-                            <p>{item.fullName}</p>
-                        </div>
-                        {[1, 2, 3, 4, 5].map((star) => (
-                            <span
-                                key={star}
-                                style={{
-                                    color: item.rate >= star ? 'gold' : 'gray',
-                                    fontSize: '24px',
-                                }}
+                        <div className="thumbnail-gallery">
+                            <button
+                                className="thumb-prev"
+                                onClick={() => setThumbsStartIndex(Math.max(0, thumbsStartIndex - 1))}
+                                disabled={thumbsStartIndex === 0}
                             >
+                                &#10094;
+                            </button>
+                            <div className="thumbs-container">
+                                <div className="thumbs-wrapper"
+                                     style={{transform: `translateX(-${thumbsStartIndex * 160}px)`}}>
+                                    {gallery.map((img, index) => (
+                                        <img key={index}
+                                             src={img}
+                                             alt="Thumbnail"
+                                             className={currentSlide === index ? "active-thumbnail" : ""}
+                                             onClick={() => selectImage(index)}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                            <button
+                                className="thumb-next"
+                                onClick={() => setThumbsStartIndex(Math.min(gallery.length - maxVisibleThumbs, thumbsStartIndex + 1))}
+                                disabled={thumbsStartIndex >= gallery.length - maxVisibleThumbs}
+                            >
+                                &#10095;
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className='information'>
+                        <h1>{product.name}</h1>
+                        <div className='price'>
+                            <p>€ {(product.price).toFixed(2)}</p>
+                            <s>€ {(product.lastPrice).toFixed(2)}</s>
+                        </div>
+                        <div className='installation'>
+                            <input type='checkbox' checked={installation} onChange={handleChangeCheckbox}></input>
+                            <p>installation + € {(product.installationPrice).toFixed(2)}</p>
+                        </div>
+                        <button onClick={() => addToCart(product.id, installation)}>ADD TO CART</button>
+                        <div className="description">
+                            <p>{product.description}</p>
+                        </div>
+                    </div>
+
+                </div>
+                <p style={{textAlign: 'center', fontSize: '25px', marginTop: '50px'}}>WHY CEDARKIDS SWING SETS?</p>
+                <ul className='whyBlock'>
+                    <li>
+                        <img src={quality}/>
+                        <h1>High Quality Design</h1>
+                        <p>We work hard to make sure your swing set features design elements as stylish as they are
+                            strong. From the materials to the colors to the overall aesthetic, these are swing sets
+                            you'll be proud to have in your backyard. </p>
+                    </li>
+                    <li>
+                        <img src={cert}/>
+                        <h1>Tested and Certified</h1>
+                        <p>All of our swing sets are certified to meet and exceed ASTM standards. We test our
+                            performance for kids up to 12-years-old, going above and beyond industry standards.</p>
+                    </li>
+                    <li>
+                        <img src={warranty}/>
+                        <h1>Warranty and Assembly</h1>
+                        <p>With a 5 Year Limited Warranty and 3D interactive assembly instructions with the BILT® app,
+                            you'll be supported from the very beginning for years and years of backyard fun.</p>
+                    </li>
+                </ul>
+                <div className='demensions'>
+                    <p>Assembled Dimensions</p>
+                    <p>{formattedDemensions}</p>
+                    <p>This product is intended for RESIDENTIAL USE ONLY. Any use of this product outside of a
+                        residential
+                        setting will make the product warranty null and void.</p>
+                </div>
+
+                <div className='composition'>
+                    <img src={product.compositionImage} alt="Composition"/>
+                    <ul>
+                        <p>Set composition:</p>
+                        {product.composition && product.composition.split("\n").map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className='reviewDetails'>
+                    <p>CUSTOMER REVIEWS</p>
+
+                    {[1, 2, 3, 4, 5].map((star) => (
+                        <span
+                            key={star}
+                            style={{
+                                color: averageRate >= star ? 'gold' : 'gray',
+                                fontSize: '24px',
+                            }}
+                        >
                               ★
                             </span>
-                        ))}
-                        <p>{item.text}</p>
-                        <ul>{item.reviewGallery.map(item => (
-
-                            <li>
-                                <img onClick={() => {
-                                    setCurrentImage(item.imageUrl);
-                                    setShowImage(true)
-                                }} src={item.imageUrl}/>
-                            </li>
-                        ))}
-                        </ul>
-                        {showImage && (
-                            <div className='imageContainer' onClick={() => setShowImage(false)}>
-                                <button className='closeButton' onClick={() => setShowImage(false)}>✕</button>
-                                <img src={currentImage} alt="Full View"/>
+                    ))}
+                    <span> /</span>
+                    <span style={{color: 'gold', fontSize: '24px'}}> ★★★★★</span>
+                    <p>{averageRate.toFixed(2)} / 5.00</p>
+                    <p>Based on {product.reviews.length} reviews</p>
+                    <button onClick={() => setShowReviewForm(!showReviewForm)}>Write a review</button>
+                </div>
+                {showReviewForm &&
+                    <ReviewForm setShowReviewForm={setShowReviewForm} product={product}/>}
+                <div className='reviews'>
+                    {product.reviews.map(item => (
+                        <div className='comment'>
+                            <div className='userInfo'>
+                                <img src={userIcon}/>
+                                <p>{item.fullName}</p>
                             </div>
-                        )}
+                            {[1, 2, 3, 4, 5].map((star) => (
+                                <span
+                                    key={star}
+                                    style={{
+                                        color: item.rate >= star ? 'gold' : 'gray',
+                                        fontSize: '24px',
+                                    }}
+                                >
+                              ★
+                            </span>
+                            ))}
+                            <p>{item.text}</p>
+                            <ul>{item.reviewGallery.map(item => (
 
-                    </div>
-                ))}
+                                <li>
+                                    <img onClick={() => {
+                                        setCurrentImage(item.imageUrl);
+                                        setShowImage(true)
+                                    }} src={item.imageUrl}/>
+                                </li>
+                            ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
--- frontend/woodlit/src/components/ReviewForm.js
+++ frontend/woodlit/src/components/ReviewForm.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import config from '../config'
 
-export default function ReviewForm({ setShowReviewForm, product }) {
+export default function ReviewForm({setShowReviewForm, product}) {
     const [galleryImages, setGalleryImages] = useState([]);
     const [rating, setRating] = useState();
     const [text, setText] = useState();
@@ -11,51 +12,53 @@ export default function ReviewForm({ setShowReviewForm, product }) {
     async function sendForm(e) {
         e.preventDefault();
         setError(null);
-        
+
         const formData = new FormData();
         formData.append("fullName", fullName);
         formData.append("text", text);
         formData.append("rate", rating);
-        formData.append("gallery", galleryImages); 
-        formData.append("productId", product.id); 
-        
+        formData.append("gallery", galleryImages);
+        formData.append("productId", product.id);
+
         galleryImages.forEach((file, index) => {
-            formData.append(`gallery`, file); 
+            formData.append(`gallery`, file);
         });
-    
+
         try {
-            await axios.post('https://cedarkids.eu/api/review/upload', formData, {
-                headers: { 'Content-Type': 'multipart/form-data' }
+            await axios.post(`${config.API_URL}/api/review/upload`, formData, {
+                headers: {'Content-Type': 'multipart/form-data'}
             });
             window.location.reload();
         } catch (error) {
             setError(error.response.data);
         }
     }
-  return (
-    <div className='reviewForm'>
+
+    return (
+        <div className='reviewForm'>
             <form onSubmit={sendForm}>
                 <p>Add new Review</p>
                 <input name='fullName' onChange={(e) => setFullName(e.target.value)} placeholder='Full name'></input>
                 <div className="rating">
-      {[1, 2, 3, 4, 5].map((star) => (
-        <span
-          key={star}
-          style={{
-            cursor: 'pointer',
-            color: rating >= star ? 'gold' : 'gray',
-            fontSize: '24px',
-          }}
-          onClick={() => setRating(star)}
-        >
+                    {[1, 2, 3, 4, 5].map((star) => (
+                        <span
+                            key={star}
+                            style={{
+                                cursor: 'pointer',
+                                color: rating >= star ? 'gold' : 'gray',
+                                fontSize: '24px',
+                            }}
+                            onClick={() => setRating(star)}
+                        >
           ★
         </span>
-      ))}
-    </div>
+                    ))}
+                </div>
                 <textarea name='text' onChange={(e) => setText(e.target.value)} placeholder='Your comment'></textarea>
-                <input type='file' multiple onChange={(e) => setGalleryImages(Array.from(e.target.files))} placeholder='Gallery'></input>
+                <input type='file' multiple onChange={(e) => setGalleryImages(Array.from(e.target.files))}
+                       placeholder='Gallery'></input>
                 <button type='submit'>Submit</button>
             </form>
         </div>
-  )
+    )
 }
